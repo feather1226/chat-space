4e5091eb7db8c3b83c0e2727490502d1bd967203
Masayuki <o8o95006237@gmail.com>
2017-11-08 16:53:47 +0900

    インデント揃え、重複処理をメソッド化
    paramsの取得をinitializeメソッドに定義。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
+
   def edit
-      @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-   if user.update(tweet_params)
-   render "messages/index"
-   else render :edit
-   end
-end
+    if @user.update(tweet_params)
+      render "messages/index"
+    else
+      render :edit
+    end
+  end
   private
   def tweet_params
      params.permit(:name, :email)
+  end
+
+  def initialize
+    @user = User.find(params[:id])
   end
 end
