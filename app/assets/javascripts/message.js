faca8f4e4633e917b84b6a685f0b052cbc6d2ead
Masayuki <o8o95006237@gmail.com>
2017-11-24 18:51:23 +0900

    メッセージ送信の非同期化
    Ajaxによりメッセージ表示を非同期化。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    var message_list = $(".RightContents__Bottom");
+    var put_image = `<div class = RightContents__Bottom--word>
+      <span class = RightContents__Bottom--word--name>
+        ${ message.username }
+      </span>
+      <span class = RightContents__Bottom--word--time>
+        ${ message.created_at }
+      </span>
+      <span class = RightContents__Bottom--word--pic>
+        image_tag ${ message.image }, alt:"picture", height: "50", width: "50"
+      </span></div>`
+    var put_text =  `<div class = RightContents__Bottom--word>
+      <span class = RightContents__Bottom--word--name>
+        ${ message.username }
+      </span>
+      <span class = RightContents__Bottom--word--time>
+        ${ message.created_at }
+      </span>
+      <span class = RightContents__Bottom--word--text>
+        ${ message.body }
+      </span></div>`
+      if ( message.body === null ) {
+        message_list.append(put_image)
+      };
+      if ( message.image.url === null ) {
+        message_list.append(put_text);
+      }
+    }
+    function flash() {
+      var html = '<div class="flash flash__notice"><span class = sentence>メッセージを送信しました</span></div>'
+      $('body').append(html);
+      $('.flash').fadeIn(500).fadeOut(5000);
+      setTimeout(function(){
+       $('.flash').remove();
+      },2500);
+    };
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $('.RightContents__Bottom--Mbox--Send').removeAttr('data-disable-with');
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          buildHTML(data);
+          $('.RightContents__Bottom--Mbox--inputword').val('');
+          $('.RightContents__Bottom--Mbox--Awesome').val('');
+          flash();
+          $('.RightContents__Bottom--Mbox--Send').on('click', function() {
+          $('.RightContents__Bottom').animate({scrollTop: 10000000 }, 'fast');
+         });
+       })
+       .fail(function() {
+         alert('メッセージを送信できません');
+      });
+      return false;
+  });
+});
