1ce941d18b7b22bcbfeadc6300f09e0774ffb1e2
Masayuki <o8o95006237@gmail.com>
2017-11-29 17:34:08 +0900

    非同期更新の修正
    画像が非同期通信にて表示されなかった問題を修正。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var message_list = $(".RightContents__Bottom");
-    var put_image = `
+    var html = `
     <div class = RightContents__Bottom--word data-message-id="${message.id}">
       <span class = RightContents__Bottom--word--name>
         ${ message.name }
@@ -10,63 +10,49 @@ $(function(){
         ${ message.data }
       </span>
       <span class = RightContents__Bottom--word--pic>
-        image_tag ${ message.image }, alt:"picture", height: "50", width: "50"
-      </span>
-    </div>`
-    var put_text = `
-    <div class = RightContents__Bottom--word data-message-id=${message.id}>
-      <span class = RightContents__Bottom--word--name>
-        ${ message.name }
-      </span>
-      <span class = RightContents__Bottom--word--time>
-        ${ message.data }
+        <img src="${ message.image.url }", height="50", width="50">
       </span>
       <span class = RightContents__Bottom--word--text>
         ${ message.body }
       </span>
     </div>`
-      if ( message.body === null ) {
-        message_list.append(put_image)
-      };
-      if ( message.image.url === null ) {
-        message_list.append(put_text);
-      }
-    }
+    message_list.append(html);
+  }
 
-    function flash() {
-      var html = '<div class="flash flash__notice"><span class = sentence>メッセージを送信しました</span></div>'
-      $('body').append(html);
-      $('.flash').fadeIn(500).fadeOut(5000);
-      setTimeout(function(){
-       $('.flash').remove();
-      },2500);
-    };
+  function flash() {
+    var html = '<div class="flash flash__notice"><span class = sentence>メッセージを送信しました</span></div>'
+    $('body').append(html);
+    $('.flash').fadeIn(500).fadeOut(5000);
+    setTimeout(function(){
+     $('.flash').remove();
+    },2500);
+  };
 
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $('.RightContents__Bottom--Mbox--Send').removeAttr('data-disable-with');
-        $.ajax({
-          url: url,
-          type: "POST",
-          data: formData,
-          dataType: 'json',
-          processData: false,
-          contentType: false
-        })
-        .done(function(data){
-          buildHTML(data);
-          $('.RightContents__Bottom--Mbox--inputword', '.RightContents__Bottom--Mbox--Awesome').val('');
-          flash();
-          $('.RightContents__Bottom--Mbox--Send').on('click', function() {
-          $('.RightContents__Bottom').animate({scrollTop: 10000000 }, 'fast');
-         });
-       })
-       .fail(function() {
-         alert('メッセージを送信できません');
-      });
-      return false;
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $('.RightContents__Bottom--Mbox--Send').removeAttr('data-disable-with');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        buildHTML(data);
+        $('.RightContents__Bottom--Mbox--inputword', '.RightContents__Bottom--Mbox--Awesome').val('');
+        flash();
+        $('.RightContents__Bottom--Mbox--Send').on('click', function() {
+        $('.RightContents__Bottom').animate({scrollTop: 10000000 }, 'fast');
+       });
+     })
+     .fail(function() {
+       alert('メッセージを送信できません');
+    });
+    return false;
   });
   setInterval(function(){
     if($('.RightContents__Bottom--word')){
